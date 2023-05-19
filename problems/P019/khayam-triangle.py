5aa1f6aaa8c8bf03e6c26d38ef267bfce88989f8
Bita Ahmadvand <Bita.ahmadvand91@gmail.com>
2023-05-19 14:18:30 +0430

    6-7-8

--- problems/P019/khayam-triangle.py
+++ problems/P019/khayam-triangle.py
@@ -1,8 +1,6 @@
 n= int(input())
 l=[1]
 print(l)
-l=[1,1]
-print(l)
 for x in range (1,n-1):
     l=[1,1]
     for y in range (0,x-1):
--- problems/P025/multiplication.py
+++ problems/P025/multiplication.py
@@ -7,8 +7,10 @@ print(n_2)
 a=0
 for x in range(1,l_2+1):
     d=0  
-    for y in range (1,l_1+1):  
-        c =int(n_2[l_2 - x ] )*int( n_1[l_1 -y])*(10**(y-1))
+    for y in range (1,l_1+1):
+        d_1 = int(n_2[l_2 - x ] )
+        d_2 =int( n_1[l_1 -y])   
+        c =**(10**(y-1))
         d=d+c
     a=a+d*(10**(x-1))
 print(a)
